213810285bd74216d2e01cc6874757683cd248ee
Reza Rahimi <reza.amd.2021@gmail.com>
2021-11-09 00:04:32 +0000

    refactored hip_gpu_kernel_helpers.h

--- jaxlib/hip_gpu_kernel_helpers.h
+++ jaxlib/hip_gpu_kernel_helpers.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 Google LLC
+/* Copyright 2021 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,54 +13,54 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef JAXLIB_CUDA_GPU_KERNEL_HELPERS_H_
-#define JAXLIB_CUDA_GPU_KERNEL_HELPERS_H_
+#ifndef JAXLIB_HIP_GPU_KERNEL_HELPERS_H_
+#define JAXLIB_HIP_GPU_KERNEL_HELPERS_H_
 
 #include <memory>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "third_party/gpus/cuda/include/cublas_v2.h"
-#include "third_party/gpus/cuda/include/cuda_runtime_api.h"
-#include "third_party/gpus/cuda/include/cusolverDn.h"
-#include "third_party/gpus/cuda/include/cusparse.h"
+#include "rocm/include/hip/hip_runtime_api.h"
+#include "rocm/include/hipblas.h"
+#include "rocm/include/hipsolver.h"
+#include "rocm/include/hipsparse.h"
 
 #define JAX_AS_STATUS(expr) jax::AsStatus(expr, __FILE__, __LINE__, #expr)
 
-#define JAX_THROW_IF_ERROR(expr)                                           \
-  {                                                                        \
-    auto s___ = (expr);                                                    \
-    if (!s___.ok()) throw std::runtime_error(std::string(s___.message())); \
+#define JAX_THROW_IF_ERROR(expr)                                               \
+  {                                                                            \
+    auto s___ = (expr);                                                        \
+    if (!s___.ok())                                                            \
+      throw std::runtime_error(std::string(s___.message()));                   \
   }
 
-#define JAX_RETURN_IF_ERROR(expr) \
-  {                               \
-    auto s___ = (expr);           \
-    if (!s___.ok()) return s___;  \
+#define JAX_RETURN_IF_ERROR(expr)                                              \
+  {                                                                            \
+    auto s___ = (expr);                                                        \
+    if (!s___.ok())                                                            \
+      return s___;                                                             \
   }
 
 namespace jax {
 
 // Used via JAX_AS_STATUS(expr) macro.
-absl::Status AsStatus(cudaError_t error, const char* file, std::int64_t line,
+absl::Status AsStatus(hipError_t error, const char* file, std::int64_t line,
                       const char* expr);
-absl::Status AsStatus(cusolverStatus_t status, const char* file,
+absl::Status AsStatus(hipsolverStatus_t status, const char* file,
                       std::int64_t line, const char* expr);
-absl::Status AsStatus(cusparseStatus_t status, const char* file,
+absl::Status AsStatus(hipsparseStatus_t status, const char* file,
                       std::int64_t line, const char* expr);
-absl::Status AsStatus(cublasStatus_t status, const char* file,
+absl::Status AsStatus(hipblasStatus_t status, const char* file,
                       std::int64_t line, const char* expr);
 
 // Builds an array of pointers to each array in a batch, in device memory.
 // Caution: the return value must be kept alive (e.g., via a stream
 // synchronization) until the copy enqueued by MakeBatchPointers on `stream`
 // completes.
-absl::StatusOr<std::unique_ptr<void*[]>> MakeBatchPointers(cudaStream_t stream,
-                                                           void* buffer,
-                                                           void* dev_ptrs,
-                                                           int batch,
-                                                           int batch_elem_size);
+absl::StatusOr<std::unique_ptr<void*[]>>
+MakeBatchPointers(hipStream_t stream, void* buffer, void* dev_ptrs, int batch,
+                  int batch_elem_size);
 
 }  // namespace jax
 
-#endif  // JAXLIB_CUDA_GPU_KERNEL_HELPERS_H_
+#endif  // JAXLIB_HIP_GPU_KERNEL_HELPERS_H_
