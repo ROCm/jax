3034a62e2f118e7c13d82d07cac64b949729b90e
Reza Rahimi <reza.amd.2021@gmail.com>
2021-11-10 18:44:10 +0000

    refactored hip_prng.py

--- jaxlib/hip_prng.py
+++ jaxlib/hip_prng.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import functools
 import itertools
 import operator
@@ -22,13 +21,14 @@ import numpy as np
 from jaxlib import xla_client
 
 try:
-  from . import _cuda_prng
-  for _name, _value in _cuda_prng.registrations().items():
-    xla_client.register_custom_call_target(_name, _value, platform="CUDA")
+  from . import _hip_prng
+  for _name, _value in _hip_prng.registrations().items():
+    xla_client.register_custom_call_target(_name, _value, platform="HIP")
 except ImportError:
   pass
 
 _prod = lambda xs: functools.reduce(operator.mul, xs, 1)
+
 
 def threefry2x32(c, keys, data):
   """ThreeFry2x32 kernel for GPU."""
@@ -42,14 +42,15 @@ def threefry2x32(c, keys, data):
     assert dims == x_shape.dimensions(), (dims, x_shape)
   ndims = len(dims)
 
-  opaque = _cuda_prng.cuda_threefry2x32_descriptor(_prod(dims))
+  opaque = _hip_prng.hip_threefry2x32_descriptor(_prod(dims))
   layout = tuple(range(ndims - 1, -1, -1))
   shape = xla_client.Shape.array_shape(dtype, dims, layout)
   return xla_client.ops.CustomCallWithLayout(
-      c, b"cuda_threefry2x32",
+      c,
+      b"hip_threefry2x32",
       operands=(keys[0], keys[1], data[0], data[1]),
       shape_with_layout=xla_client.Shape.tuple_shape([shape, shape]),
-      operand_shapes_with_layout=(shape,) * 4,
+      operand_shapes_with_layout=(shape, ) * 4,
       opaque=opaque,
-      api_version=xla_client.ops.CustomCallApiVersion
-      .API_VERSION_STATUS_RETURNING)
+      api_version=xla_client.ops.CustomCallApiVersion.
+      API_VERSION_STATUS_RETURNING)
