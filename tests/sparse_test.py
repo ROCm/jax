475bb6d17b5f43e164fbafe917f3075d571f3eef
Rahul Batra <you@example.com>
2023-03-28 18:13:39 +0000

    [ROCm]: Disable sparse tests

--- tests/sparse_test.py
+++ tests/sparse_test.py
@@ -354,6 +354,7 @@ class cuSparseTest(sptu.SparseTestCase):
     dtype=all_dtypes,
     transpose=[True, False],
   )
+  @jtu.skip_on_devices("rocm")
   def test_csr_matvec(self, shape, dtype, transpose):
     op = lambda M: M.T if transpose else M
 
@@ -374,6 +375,7 @@ class cuSparseTest(sptu.SparseTestCase):
     dtype=all_dtypes,
     transpose=[True, False],
   )
+  @jtu.skip_on_devices("rocm")
   def test_csr_matmat(self, shape, dtype, transpose):
     op = lambda M: M.T if transpose else M
 
@@ -688,6 +690,7 @@ class cuSparseTest(sptu.SparseTestCase):
     transpose=[True, False],
   )
   @unittest.skipIf(not GPU_LOWERING_ENABLED, "test requires cusparse/hipsparse")
+  @jtu.skip_on_devices("rocm")
   def test_coo_spmv(self, shape, dtype, transpose):
     rng_sparse = rand_sparse(self.rng())
     rng_dense = jtu.rand_default(self.rng())
@@ -711,6 +714,7 @@ class cuSparseTest(sptu.SparseTestCase):
     transpose=[True, False],
   )
   @unittest.skipIf(not GPU_LOWERING_ENABLED, "test requires cusparse/hipsparse")
+  @jtu.skip_on_devices("rocm")
   def test_coo_spmm(self, shape, dtype, transpose):
     rng_sparse = rand_sparse(self.rng())
     rng_dense = jtu.rand_default(self.rng())
@@ -734,6 +738,7 @@ class cuSparseTest(sptu.SparseTestCase):
     transpose=[True, False],
   )
   @unittest.skipIf(not GPU_LOWERING_ENABLED, "test requires cusparse/hipsparse")
+  @jtu.skip_on_devices("rocm")
   def test_csr_spmv(self, shape, dtype, transpose):
     rng_sparse = rand_sparse(self.rng())
     rng_dense = jtu.rand_default(self.rng())
@@ -755,6 +760,7 @@ class cuSparseTest(sptu.SparseTestCase):
     transpose=[True, False],
   )
   @unittest.skipIf(not GPU_LOWERING_ENABLED, "test requires cusparse/hipsparse")
+  @jtu.skip_on_devices("rocm")
   def test_csr_spmm(self, shape, dtype, transpose):
     rng_sparse = rand_sparse(self.rng())
     rng_dense = jtu.rand_default(self.rng())
