b1c6f0cea0abbe19d5ad6280b259d70ebcf8de79
pramenku <7664080+pramenku@users.noreply.github.com>
2024-07-12 20:09:44 +0530

    Delete setup.internal.rocm.sh

--- setup.internal.rocm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#==============================================================================
-#
-# setup.internal.rocm.sh: Prepare the internal ROCM installation on the container.
-# Usage: setup.internal.rocm.sh <ROCM_VERSION>
-set -x
-
-ROCM_MAJ_MIN=$1
