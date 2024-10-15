2159972879730b4c528386b77a07fb3399e053f9
Mathew Odden <mathew.odden@amd.com>
2024-10-15 17:04:45 -0500

    [ROCm] Fix invalid command in dev build script
    When bazel options were empty, the args to build/build.py were
    leaving an empty string arg which is invalid.

--- build/rocm/dev_build_rocm.py
+++ build/rocm/dev_build_rocm.py
@@ -83,8 +83,10 @@ def build_jax_xla(xla_path, rocm_version, rocm_target, use_clang, clang_path):
         f"--use_clang={str(use_clang).lower()}",
         f"--rocm_amdgpu_targets={rocm_target}",
         f"--rocm_path=/opt/rocm-{rocm_version}/",
-        bazel_options,
     ]
+
+    if bazel_options:
+        build_command.append(bazel_options)
 
     if clang_option:
         build_command.append(clang_option)
