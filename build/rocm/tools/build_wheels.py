b2f491e0705dab150baf55e34b95e6d525aedf2c
Charles Hofer <Charles.Hofer@amd.com>
2024-12-13 10:51:33 -0600

    Remove trailing spaces

--- build/rocm/tools/build_wheels.py
+++ build/rocm/tools/build_wheels.py
@@ -315,7 +315,7 @@ def main():
     # make the wheels delete-abl by the runner
     whl_house = os.path.join(args.jax_path, "wheelhouse")
     logging.info(f'Changing permissions for {whl_house}')
-    mode = (stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | 
+    mode = (stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
             stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
             stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH )
     for item in os.listdir(whl_house):
