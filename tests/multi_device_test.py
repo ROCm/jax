c5ee54f979dad478dd10a4c151877fb020af2396
Ruturaj Vaidya <ruturaj.vaidya@amd.com>
2025-02-26 10:17:54 -0600

    Fix bytes_limit key of mem_stats (#243)

--- tests/multi_device_test.py
+++ tests/multi_device_test.py
@@ -312,7 +312,7 @@ class MultiDeviceTest(jtu.JaxTestCase):
       self.skipTest('Only can run test on device with mem_stats')
     mesh = Mesh(devices, axis_names=("i"))
     sharding = NamedSharding(mesh, P('i'))
-    available_memory = mem_stats['bytes_reservable_limit']
+    available_memory = mem_stats['bytes_limit']
     array_size = available_memory // (6 * len(devices)) * len(devices)
     # Set up tracemalloc to track memory usage.
     tm.start()
