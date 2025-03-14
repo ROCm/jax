f6a1603ae2a11101b4ec02d1579f99d4bea613da
Charles Hofer <Charles.Hofer@amd.com>
2025-03-14 03:06:52 +0000

    Remove extra newline

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,4 +12,3 @@ def pytest_json_modifyreport(json_report):
             and "total" in json_report["summary"]):
         json_report["summary"]["unskipped_total"] = json_report["summary"]["total"] - json_report["summary"].get("skipped")
         del json_report["summary"]["total"]
-
