6a2e3a743de75e975780d67227633d9eddddfc18
Charles Hofer <Charles.Hofer@amd.com>
2025-04-22 21:12:59 +0000

    Get branch name from Github in XLA update script

--- build/rocm/tools/update_xla_hash.py
+++ build/rocm/tools/update_xla_hash.py
@@ -4,18 +4,39 @@ import argparse
 import logging
 import os.path
 import re
+import requests
 import subprocess
 
+GH_COMMIT_URL = "https://api.github.com/repos/{0}/commits/{1}"
+GH_BASE_URL = "https://github.com"
 logger = logging.getLogger(__name__)
 
 
-def update_xla_hash(xla_commit, xla_repo, workspace_file_path):
+def update_xla_hash(xla_commit, xla_repo, workspace_file_path, gh_token):
     # Verify that the workspace_file exists
     if not os.path.isfile(workspace_file_path):
         raise ValueError(f"Workspace file '{workspace_file}' does not exist")
 
-    # Convert xla_commit to a commit hash if it's a branch
-    xla_commit_hash = xla_commit
+    # If we were given a GH auth token, use it to make sure that the commit
+    # exists and convert a branch name to a commit hash
+    if gh_token:
+        logger.debug(GH_COMMIT_URL.format(xla_repo, xla_commit))
+        commit_info_resp = requests.get(
+            url=GH_COMMIT_URL.format(xla_repo, xla_commit),
+            headers={
+                "Accept": "application/vnd.github.sha",
+                "Authorization": f"Bearer {gh_token}",
+                "X-Github-Api-Version": "2022-11-28",
+            }
+        )
+        commit_info_resp.raise_for_status()
+        logger.info("Found commit hash via GH API: %s", commit_info_resp.text)
+        xla_commit_hash = commit_info_resp.text
+    # If the user didn't give us a token make sure the commit hash looks hashy
+    else:
+        if not xla_commit.isalnum():
+            raise ValueError(f"XLA commit hash '{xla_commit}' is not a valid commit hash")
+        xla_commit_hash = xla_commit
 
     # Get the sha256 of this commit
     curl_proc = subprocess.Popen(
@@ -24,7 +45,7 @@ def update_xla_hash(xla_commit, xla_repo, workspace_file_path):
             "--output",
             "-",
             "-L",
-            f"{xla_repo}/archive/{xla_commit_hash}.tar.gz",
+            f"{GH_BASE_URL}/{xla_repo}/archive/{xla_commit_hash}.tar.gz",
         ],
         stdout=subprocess.PIPE,
     )
@@ -58,7 +79,7 @@ def update_xla_hash(xla_commit, xla_repo, workspace_file_path):
         )
         contents = re.sub(
             'tf_mirror_urls\("[a-zA-Z0-9:/.]+/archive',
-            f'tf_mirror_urls\("{xla_repo}/archive',
+            f'tf_mirror_urls("{GH_BASE_URL}/{xla_repo}/archive',
             contents,
             flags=re.M,
         )
@@ -77,20 +98,30 @@ def parse_args():
         help="Branch or commit to put in the workspace file",
     )
     arg_parser.add_argument(
+        "gh_token",
+        help="Github token to authenticate with. Either the GIHUB_TOKEN from Actions or your PAT."
+    )
+    arg_parser.add_argument(
         "--xla-repo",
-        default="https://github.com/openxla/xla",
-        help="URL to the XLA repo where this branch or commit can be "
-        "found. Defaults to https://github.com/openxla/xla.",
+        default="openxla/xla",
+        help="The repo where this branch or commit can be found. Should be in the form of <owner>/<repo>. Defaults to openxla/xla.",
     )
     arg_parser.add_argument(
         "--workspace-file",
         default="./third_party/xla/workspace.bzl",
-        help="Path to the workspace.bzl file to put the hash. Defaults to "
-        "./third_party/xla/workspace.bzl.",
+        help="Path to the workspace.bzl file to put the hash. Defaults to ./third_party/xla/workspace.bzl.",
+    )
+    arg_parser.add_argument(
+        "-v", "--verbose",
+        help="Turn on debug logging",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG
     )
     return arg_parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    update_xla_hash(args.xla_commit, args.xla_repo, args.workspace_file)
+    logging.basicConfig(level=args.loglevel)
+    update_xla_hash(args.xla_commit, args.xla_repo, args.workspace_file, args.gh_token)
