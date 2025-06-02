ec6644ddb2e3d55013ce733cc10aadf2c960ecd1
Pham Binh <phambinh@w.amd.com>
2025-06-02 16:50:11 +0000

    Select the platform GPUs for a few MultiProcessGpuTest test cases.

--- tests/multiprocess_gpu_test.py
+++ tests/multiprocess_gpu_test.py
@@ -60,6 +60,7 @@ class MultiProcessGpuTest(jtu.JaxTestCase):
         env["JAX_PORT"] = str(port)
         env["NUM_TASKS"] = str(num_tasks)
         env["TASK"] = str(task)
+        env["JAX_PLATFORMS"] = 'gpu'
         if jtu.is_device_rocm():
           env["HIP_VISIBLE_DEVICES"] = ",".join(
               str((task * num_gpus_per_task) + i) for i in range(num_gpus_per_task))
@@ -106,6 +107,7 @@ class MultiProcessGpuTest(jtu.JaxTestCase):
         env["JAX_PORT"] = str(port)
         env["NUM_TASKS"] = str(num_tasks)
         env["TASK"] = str(task)
+        env["JAX_PLATFORMS"] = 'gpu'
         visible_devices = ",".join(
             str((task * num_gpus_per_task) + i) for i in range(num_gpus_per_task))
 
