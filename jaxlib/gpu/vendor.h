8fc7e2b7073d39541ed90a1b9e4aed557d05c835
Pham Binh <phamhuuthanh.binh@amd.com>
2025-06-05 03:20:19 +0300

    [0.6.0-UT]  Fix NumpyLinalgTest test cases (#453)
    Co-authored-by: Pham Binh <phambinh@w.amd.com>
    (cherry picked from commit 29c392d03e9eb217a33bb73046f4b508750140c1)

--- jaxlib/gpu/vendor.h
+++ jaxlib/gpu/vendor.h
@@ -422,7 +422,7 @@ constexpr uint32_t kNumThreadsPerWarp = 32;
 }  // namespace jax::JAX_GPU_NAMESPACE
 
 #elif defined(JAX_GPU_HIP)
-
+#define HIPBLAS_V2 1
 // IWYU pragma: begin_exports
 #include "rocm/include/hip/hip_cooperative_groups.h"
 #include "rocm/include/hip/hip_runtime_api.h"
@@ -442,12 +442,12 @@ constexpr uint32_t kNumThreadsPerWarp = 32;
 // TODO(Ruturaj4): Currently equivalent API does exist in
 // MIOpen lib. Remove when MIOpen support is complete.
 #define MIOPEN_STATUS_SUCCESS 0
-
+// TODO(PhamBinh): Figure out later to use hipComplex or hipFloatComplex
 typedef hipFloatComplex gpuComplex;
 typedef hipDoubleComplex gpuDoubleComplex;
 
-typedef hipblasComplex gpublasComplex;
-typedef hipblasDoubleComplex gpublasDoubleComplex;
+typedef hipFloatComplex gpublasComplex;
+typedef hipDoubleComplex gpublasDoubleComplex;
 typedef hipsolverHandle_t gpusolverDnHandle_t;
 typedef hipblasFillMode_t gpublasFillMode_t;
 typedef hipsolverFillMode_t gpusolverFillMode_t;
