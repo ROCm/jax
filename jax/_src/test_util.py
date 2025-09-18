2feb8d0a60337f89661fcfb41e5467a09bc4c505
gulsumgudukbay <gudukbay@gmail.com>
2025-09-18 16:42:01 +0000

    adding third field to return value of get_rocm_version

--- jax/_src/test_util.py
+++ jax/_src/test_util.py
@@ -414,8 +414,8 @@ def get_rocm_version():
   version_path = Path(rocm_path) / ".info" / "version"
   try:
     version_str = version_path.read_text().strip()
-    major, minor, *_ = version_str.split(".")
-    return int(major), int(minor)
+    major, minor, patch = version_str.split(".")
+    return int(major), int(minor), int(patch)
   except FileNotFoundError:
     raise unittest.SkipTest("ROCm was not installed")
 
