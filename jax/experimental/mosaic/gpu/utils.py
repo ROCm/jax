ff14795b6f287a7391fc5aab247bcd9006d6e030
Aleksei Rechinskii <Aleksei.Rechinskii@amd.com>
2026-01-20 13:52:24 +0000

    stubify bar.sync too for ROCm

--- jax/experimental/mosaic/gpu/utils.py
+++ jax/experimental/mosaic/gpu/utils.py
@@ -935,13 +935,18 @@ def warpgroup_barrier():
   # gpu.barrier() uses barrier number 0, and it would be unsafe to reuse it,
   # so we shift the warpgroup index by 1.
   i32 = ir.IntegerType.get_signless(32)
-  llvm.inline_asm(
-      ir.Type.parse("!llvm.void"),
-      [arith.addi(warpgroup_idx(sync=False), c(1, i32))],
-      f"bar.sync $0, {WARPGROUP_SIZE};",
-      "r",
-      has_side_effects=True,
-  )
+
+  if IS_ROCM:
+    # TODO(Arech) FIX BEFORE THE PR. This is only a stub
+    pass
+  else:
+    llvm.inline_asm(
+        ir.Type.parse("!llvm.void"),
+        [arith.addi(warpgroup_idx(sync=False), c(1, i32))],
+        f"bar.sync $0, {WARPGROUP_SIZE};",
+        "r",
+        has_side_effects=True,
+    )
 
 def warp_barrier():
   nvvm.bar_warp_sync(c(0xffffffff, ir.IntegerType.get_signless(32)))
