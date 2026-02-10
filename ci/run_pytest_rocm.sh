8ff1e9d23db2a7185adc3f7e53717f2f29b5625b
Marco Minutoli <marco.minutoli@amd.com>
2026-02-10 12:56:01 -0800

    fixup MIOpen issue

--- ci/run_pytest_rocm.sh
+++ ci/run_pytest_rocm.sh
@@ -115,6 +115,7 @@ echo "Running ROCm tests..."
 # TODO: Verify if CSV/HTML report generation should be kept (unique to ROCm)
 # TODO: Verify if log file output should be kept (unique to ROCm)
 export NPROC=32
+export MIOPEN_FIND_ENFORCE=SEARCH_DB_UPDATE
 "$JAXCI_PYTHON" -m pytest -n $num_processes --tb=short \
 tests \
 --deselect=tests/multi_device_test.py::MultiDeviceTest::test_computation_follows_data \
