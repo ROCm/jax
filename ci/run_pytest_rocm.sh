cee583f8196333373716196a8ef4cfb9887f6847
tsrw2048 <239799652+tsrw2048@users.noreply.github.com>
2026-06-09 14:17:58 +0000

    Revert "Disable hipblaslt in TheRock to avoid current Tensile issues."
    This reverts commit 368925eb79c2ede265efde86598978aba98b981e.

--- ci/run_pytest_rocm.sh
+++ ci/run_pytest_rocm.sh
@@ -101,11 +101,7 @@ echo "Final number of processes to run: $num_processes"
 export JAX_ENABLE_ROCM_XDIST="$gpu_count"
 export XLA_PYTHON_CLIENT_ALLOCATOR=platform
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
-if command -v rocm-sdk &>/dev/null; then
-  export XLA_FLAGS="--xla_gpu_force_compilation_parallelism=1 --xla_gpu_enable_nccl_comm_splitting=false --xla_gpu_enable_command_buffer= --xla_gpu_enable_cublaslt=false"
-else
-  export XLA_FLAGS="--xla_gpu_force_compilation_parallelism=1 --xla_gpu_enable_nccl_comm_splitting=false --xla_gpu_enable_command_buffer="
-fi
+export XLA_FLAGS="--xla_gpu_force_compilation_parallelism=1 --xla_gpu_enable_nccl_comm_splitting=false --xla_gpu_enable_command_buffer="
 
 # ==============================================================================
 # Run tests
