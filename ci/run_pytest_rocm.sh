f523631f17b68a119ea49405b4c426ba90664d4a
Pelumi Sodeinde <osodeind@amd.com>
2026-08-11 13:12:28 +0100

    Trying 3 processes

--- ci/run_pytest_rocm.sh
+++ ci/run_pytest_rocm.sh
@@ -64,7 +64,7 @@ export gpu_count=$(rocminfo | egrep -c "Device Type:\s+GPU")
 echo "Number of GPUs detected: $gpu_count"
 
 # export num_processes=$((gpu_count * 4))
-export num_processes=4
+export num_processes=3
 echo "Number of processes to run: $num_processes"
 
 export JAX_ENABLE_ROCM_XDIST="$gpu_count"
